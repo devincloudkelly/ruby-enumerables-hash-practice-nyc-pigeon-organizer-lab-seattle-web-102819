09ab17a65178ca2488fa11e349cf871d93ad63d9
Devin Kelly <devin.cloud.kelly@gmail.com>
2019-10-15 18:15:14 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeon_list = {}
+  data.each do |property, hash|
+    hash.each do |attribute, array|
+      array.each do |name|
+        if !pigeon_list.has_key?(name)
+          pigeon_list[name] = {}
+        end
+         if !pigeon_list[name][property]
+          pigeon_list[name][property] = []
+          
+        end
+        pigeon_list[name][property].push(attribute.to_s)
+      end
+    end
+  end
+  pigeon_list
 end
