ebac197f0b77534779e94de54fbfee2ad6259a69
Devin Kelly <devin.cloud.kelly@gmail.com>
2019-10-15 17:32:21 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,20 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeon_list = {}
+  data.each do |property, hash|
+    hash.each do |attribute, array|
+      array.each do |name|
+        if !pigeon_list.has_key?(name)
+          pigeon_list[name] = {
+            color: [],
+            gender: [],
+            lives: []
+          }
+        end
+      end
+    end
+  end
+  
+
+  pigeon_list
 end
